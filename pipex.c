36e83596729f97d2f307580f9e5bd409848d9454
Tuan Tiprez <ttiprez@f1br5s5.clusters.42paris.fr>
2025-12-03 18:45:35 +0100

    Modifications majeures : 	- Creation fonction qui cherche dans le path dans les variables d'environnements 	- Creation fonction qui cherche le chemin de la commande 	- Creation fonction qui ecrit le contenu de pipefd[0] dans output_fd 	- Creation fonction qui ecrit le retour de la commande execute dans pipefd[1] 	- Creation fonction reserve a l'execution des commandes
    Modifications mineures :
    	- Optimisation visuelle du code.
    
    A faire :
    	- Gerer tout les mallocs qui ne sont actuellement pas gerer (simples exit)
    	- Rajouter l'action de la deuxieme commande a partir de se que renvoie la premiere
    	- Separation des fonctions dans des fichiers pour une meilleur lecture visuelle
    	- Mise en norme du projet
    	- Realistion du bonus
    	- Gestion des cas d'erreurs proprement

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: ttiprez <ttiprez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 05:07:31 by ttiprez           #+#    #+#             */
-/*   Updated: 2025/12/03 03:57:04 by ttiprez          ###   ########.fr       */
+/*   Updated: 2025/12/03 18:45:05 by ttiprez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,54 +38,128 @@ void	free_split(char **split)
 	free(split);
 }
 
-bool	first_child_actions(char **argv, int pipefd[2])
+char	*find_path(char **envp)
 {
-	char	*cmd1;
-	char	**cmd1argv;
-	int		input_fd;
+	int		i;
 
-	input_fd = open(argv[1], O_RDONLY);
-	if (input_fd == -1)
-		return (perror(argv[1]), false);
-	dup2(pipefd[1], STDOUT_FILENO);
-	close(pipefd[0]);
-	close(pipefd[1]);
-	dup2(input_fd, STDIN_FILENO);
-	close(input_fd);
-	cmd1argv = ft_split(argv[2], ' ');
-	if (!cmd1argv)
-		return (perror(argv[1]), false);
-	cmd1 = malloc(ft_strlen(cmd1argv[0]) + 6);
-	if (!cmd1)
-		return (perror(cmd1argv[0]), free_split(cmd1argv), false);
-	ft_strcpy(cmd1, "/bin/");
-	ft_strcat(cmd1, cmd1argv[0]);
-	execve(cmd1, cmd1argv, NULL);
-	perror("execve");
-	return (free(cmd1), free_split(cmd1argv), false);
+	i = -1;
+	while (envp[++i])
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			return (envp[i] + 5);
+	return (NULL);
 }
 
-int main(int argc, char const *argv[])
+char	*find_cmd_path(char *cmd, char *path)
 {
+	char	**splitted_path;
+	char	*cmd_path;
+	char	*tmp;
+	int		i;
+
+	splitted_path = ft_split(path, ':');
+	if (!splitted_path)
+		exit (EXIT_FAILURE);
+	i = -1;
+	while (splitted_path[++i])
+	{
+		tmp = ft_strjoin(splitted_path[i], "/");
+		if (!tmp)
+			exit (EXIT_FAILURE);
+		cmd_path = ft_strjoin(tmp, cmd);
+		if (!cmd_path)
+			exit (EXIT_FAILURE);
+		free(tmp);
+		if (access(cmd_path, X_OK) == 0)
+		{
+			free_split(splitted_path);
+			return (cmd_path);
+		}
+		free(cmd_path);
+	}
+	return (free_split(splitted_path), NULL);
+}
+
+bool	command(char *cmd_path, char **cmdargv)
+{
+	execve(cmd_path, cmdargv, NULL);
+	perror("execve");
+	exit(EXIT_FAILURE);
+}
+
+bool	write_cmd_in_pipe(char *file, char **cmdargv, char *path, int pipefd[2])
+{
+	int		input_fd;
+
+	if (!cmdargv || !path)
+		exit (EXIT_FAILURE);
+	close(pipefd[0]);
+	input_fd = open(file, O_RDONLY);
+	if (input_fd == -1)
+		return (perror(file), false);
+	dup2(pipefd[1], STDOUT_FILENO);
+	dup2(input_fd, STDIN_FILENO);
+	close(pipefd[1]);
+	close(input_fd);
+	if (!command(find_cmd_path(cmdargv[0], path), cmdargv))
+		return (false);
+	return (true);
+}
+
+bool	write_pipe_in_fd(char *filename, int pipefd[2])
+{
+	char	buf[1024];
+	int		nb_read;
 	int		output_fd;
+
+	close(pipefd[1]);
+	output_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (output_fd == -1)
+		return (perror(filename), false);
+	nb_read = read(pipefd[0], buf, sizeof(buf));
+	while (nb_read > 0)
+	{
+		write(output_fd, buf, nb_read);
+		nb_read = read(pipefd[0], buf, sizeof(buf));
+	}
+	close(pipefd[0]);
+	close(output_fd);
+	return (true);
+}
+
+bool	child2_action(char **argv, int pipefd[2])
+{
+	if (!write_pipe_in_fd(argv[4], pipefd))
+		return (false);
+	return (true);
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
 	int		pipefd[2];
 	pid_t	child;
+	pid_t	child2;
 
 	if (argc != 5)
-		return (print_error("Error: Wrong number of arguments\n"));
+		return (print_error("Error: Wrong number of arguments (5 needed)\n"));
 	if (access(argv[1], R_OK) == -1)
 		return (perror(argv[1]), EXIT_FAILURE);
+	if (pipe(pipefd) == -1)
+		return (perror("pipe"), EXIT_FAILURE);
 	child = fork();
 	if (child == -1)
 		return (perror("fork"), EXIT_FAILURE);
 	if (child == 0)
-		if (!first_child_actions((char **)argv, pipefd))
+		if (!write_cmd_in_pipe(argv[1], ft_split(argv[2], ' '), find_path(envp), pipefd))
 			return (EXIT_FAILURE);
-	wait(NULL);
-	ft_printf("ici\n");
-	output_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (output_fd == -1)
-		return (perror(argv[4]), EXIT_FAILURE);
-	close(output_fd);
+	child2 = fork();
+	if (child2 == -1)
+		return (perror("fork"), EXIT_FAILURE);
+	if (child2 == 0)
+		if (!child2_action(argv, pipefd))
+			return (EXIT_FAILURE);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(child, NULL, 0);
+	waitpid(child2, NULL, 0);
 	return (EXIT_SUCCESS);
 }
